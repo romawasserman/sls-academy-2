763a9a2303053f6e01afbe44c0bfa195f68280cf
Roman <romawasserman@gmail.com>
2023-06-03 14:36:21 +0300

    3 task

---
+++ 03_cli_telegram_console_sender/logic.js
@@ -0,0 +1,70 @@
+import fs from 'node:fs'
+import { bot } from './test.js'
+export function storeChatId(chatId, callback) {
+  try {
+    const data = fs.readFileSync("chatIds.txt", "utf8")
+    const chatIds = data.trim().split("\n")
+
+    if (!chatIds.includes(chatId.toString())) {
+      fs.appendFileSync("chatIds.txt", `${chatId}\n`)
+    }
+
+    callback()
+  } catch (err) {
+    console.error("Error reading or storing chat ID:", err)
+  }
+}
+
+async function sendMessageToChat(chatId, message) {
+  await bot.sendMessage(chatId, message)
+}
+
+export async function sendAllUsersMessage(message) {
+  try {
+    const data = fs.readFileSync("chatIds.txt", "utf8")
+    const chatIds = data.trim().split("\n")
+
+    const sentChatIds = new Set()
+
+    for (const chatId of chatIds) {
+      if (!sentChatIds.has(chatId)) {
+        await sendMessageToChat(chatId, message)
+        sentChatIds.add(chatId)
+      }
+    }
+
+    console.log("Message sent to all users.")
+  } catch (err) {
+    console.error("Please type /start in telegram bot")
+  }
+}
+
+async function sendPhotoToChat(chatId, photo) {
+  try {
+    await bot.sendPhoto(chatId, photo)
+  } catch (error) {
+    console.error("please type /start in telegram bot")
+  }
+}
+
+export async function sendAllUsersPhoto(imagePath) {
+  try {
+    const data = fs.readFileSync("chatIds.txt", "utf8")
+    const chatIds = data.trim().split("\n")
+
+    const sentChatIds = new Set()
+
+    for (const chatId of chatIds) {
+      if (!sentChatIds.has(chatId)) {
+        try {
+          await sendPhotoToChat(chatId, imagePath)
+          sentChatIds.add(chatId)
+        } catch (error) {
+          console.error("please press /start in telegram")
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error sending photo to all users:", error)
+  }
+}
---
+++ 03_cli_telegram_console_sender/test.js
@@ -0,0 +1,72 @@
+import TelegramBot from "node-telegram-bot-api"
+import { program } from "commander"
+import { sendAllUsersMessage, sendAllUsersPhoto, storeChatId } from "./logic.js"
+
+const token = "6293864039:AAHNshlJK2Voyz8dHZxNnYgkEJlGEJ2GKzk"
+
+export const bot = new TelegramBot(token, { polling: true })
+
+program
+  .name("CLI: TELEGRAM CONSOLE SENDER")
+  .description("An application for sending text or photo to Telegram Bot")
+  .version("1.3.3.7")
+
+program
+  .command("message")
+  .description("Send message from console to Telegram Bot")
+  .argument("message")
+  .alias("m")
+  .action(async (message) => {
+    if (!message) {
+      console.log('Enter message');
+      process.exit()
+    }else {
+      let startCommandReceived = false;
+      bot.onText(/\/start/, async (msg) => {
+        if (!startCommandReceived) {
+          startCommandReceived = true;
+        storeChatId(msg.chat.id, async () => {
+          await sendAllUsersMessage(message)
+          process.exit()
+        })
+      }})
+      setTimeout(async () => {
+        if (!startCommandReceived) {
+          await sendAllUsersMessage(message);
+          process.exit();
+        }
+      }, 500);
+    }
+  })
+
+program
+  .command("photo")
+  .description(
+    "Send image to telegramm. Write path or just drag'n'drop to terminal"
+  )
+  .arguments("<path>")
+  .alias("p")
+  .action(async (path) => {
+    let startCommandReceived = false; 
+
+
+    bot.onText(/\/start/, async (msg) => {
+      if (!startCommandReceived) {
+        startCommandReceived = true;
+        storeChatId(msg.chat.id, async () => {
+          await sendAllUsersPhoto(path);
+          process.exit();
+        });
+      }
+    });
+
+
+    setTimeout(async () => {
+      if (!startCommandReceived) {
+        await sendAllUsersPhoto(path);
+        process.exit();
+      }
+    }, 500);
+  });
+
+program.parse(process.argv)
